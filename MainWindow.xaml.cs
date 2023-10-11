6fc0d528849526963772a54a521aa83d619e0503
boris_from_south <sashaborisenko@tut.by>
2023-10-11 17:15:04 +0300

    Test get Data from DB
    Test get Data from DB

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Windows;
 
 namespace ZooManager
@@ -8,11 +11,41 @@ namespace ZooManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection sqlConnection;
+
         public MainWindow()
         {
             InitializeComponent();
 
             string connectionString = ConfigurationManager.ConnectionStrings["ZooManager.Properties.Settings.TrainingDBConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+
+            ShowZooData();
+        }
+        
+        private void ShowZooData()
+        {
+            try
+            {
+                string query = "SELECT * FROM Zoo";
+
+                // SqlDataAdapter представляет своеобразный интерфейс для работы с таблицами БД посредством объектов C#
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    DataTable dataTable = new DataTable();
+                    sqlDataAdapter.Fill(dataTable);
+
+                    listZoo.DisplayMemberPath = "Location"; // Какая инфа должна отображаться
+                    listZoo.SelectedValuePath = "Id"; // Что выбирается под копотом
+                    listZoo.ItemsSource = dataTable.DefaultView;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
