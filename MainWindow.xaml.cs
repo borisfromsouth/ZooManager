fd73e64dfef92bd25d279af61ab47f25493335c7
boris_from_south <sashaborisenko@tut.by>
2023-11-08 17:00:33 +0300

    add: dependent sql request
    Create changeble request in sql DB

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,7 +2,9 @@
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Security.Cryptography;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace ZooManager
 {
@@ -11,23 +13,28 @@ namespace ZooManager
     /// </summary>
     public partial class MainWindow : Window
     {
-        SqlConnection sqlConnection;
+        SqlConnection sqlConnection; // подключение к БД
 
         public MainWindow()
         {
             InitializeComponent();
 
+            // ConfigurationManager предоставляет доступ к файлам конфигурации для клиентских приложений
+            // Программа берет нужный источник данных из добавленных "Источников данных"
             string connectionString = ConfigurationManager.ConnectionStrings["ZooManager.Properties.Settings.TrainingDBConnectionString"].ConnectionString;
             sqlConnection = new SqlConnection(connectionString);
 
+
             ShowZooData();
+            listZoo.SelectedValue = 1;
         }
         
         private void ShowZooData()
         {
+            // Для работы с БД рекомендуется испрользовать try-catch
             try
             {
-                string query = "SELECT * FROM Zoo";
+                string query = "SELECT * FROM Zoo"; // запрос в БД
 
                 // SqlDataAdapter представляет своеобразный интерфейс для работы с таблицами БД посредством объектов C#
                 SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
@@ -37,15 +44,50 @@ namespace ZooManager
                     DataTable dataTable = new DataTable();
                     sqlDataAdapter.Fill(dataTable);
 
-                    listZoo.DisplayMemberPath = "Location"; // Какая инфа должна отображаться
-                    listZoo.SelectedValuePath = "Id"; // Что выбирается под копотом
-                    listZoo.ItemsSource = dataTable.DefaultView;
+                    listZoo.DisplayMemberPath = "Location";      // Что из таблицы выводится в интерфейс
+                    listZoo.SelectedValuePath = "Id";            // Что передвется когда выбран предмет из сипска
+                    listZoo.ItemsSource = dataTable.DefaultView; // Указываем источник данных 
                 }
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+
+        private void ShowAnimalData()
+        {
+            // Для работы с БД рекомендуется испрользовать try-catch
+            try
+            {
+                string query = "SELECT * FROM Animal a inner join ZooAnimal za ON a.Id = za.AnimalId WHERE za.ZooId = @ZooId"; // запрос в БД
+
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                // SqlDataAdapter представляет своеобразный интерфейс для работы с таблицами БД посредством объектов C#
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                using (sqlDataAdapter)
+                {
+                    sqlCommand.Parameters.AddWithValue("@ZooId", listZoo.SelectedValue);
+
+                    DataTable animalDataTable = new DataTable();
+                    sqlDataAdapter.Fill(animalDataTable);
+
+                    listAnimals.DisplayMemberPath = "Name";      // Что из таблицы выводится в интерфейс
+                    listAnimals.SelectedValuePath = "Id";            // Что передвется когда выбран предмет из сипска
+                    listAnimals.ItemsSource = animalDataTable.DefaultView; // Указываем источник данных 
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void listZoo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //MessageBox.Show(listZoo.SelectedValue.ToString());
+            ShowAnimalData();
         }
     }
 }
